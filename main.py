5604792e05d7caf6146641e9a61f5a82be095a92
Harry Berg <harryaberg@gmail.com>
2022-12-06 23:30:02 +0000

    Otimises and cleans implementation. Also adds label for prepositions

--- main.py
+++ main.py
@@ -5,132 +5,84 @@ from tokenizers.models import WordPiece, WordLevel
 from tokenizers import Tokenizer, normalizers
 from tokenizers.trainers import WordLevelTrainer
 from transformers import BertModel, BertTokenizer
-from time import time
 import pandas as pd
-# %%
-train_corpus = "pythonwiki.txt"
+from word_types.prepositions import prepositions
+# %% CUSTOM TOKENISER
+# train_corpus = "pythonwiki.txt"
 
+# def get_tokenizer():
+#     tokenizer = Tokenizer(WordLevel())
+#     tokenizer.pre_tokenizer = Whitespace()
+#     tokenizer.normalizer = normalizers.Sequence([
+#         normalizers.Lowercase()
+#     ])
+#     trainer = WordLevelTrainer()
+#     tokenizer.train([train_corpus], trainer)
+#     return tokenizer
 
-def get_tokenizer():
-    tokenizer = Tokenizer(WordLevel())
-    tokenizer.pre_tokenizer = Whitespace()
-    tokenizer.normalizer = normalizers.Sequence([
-        normalizers.Lowercase()
-    ])
-    trainer = WordLevelTrainer()
-    tokenizer.train([train_corpus], trainer)
-    return tokenizer
+# tokenizer = get_tokenizer()
 
-# BERT
-
-
-tokenizer = get_tokenizer()
-
-# %%
-
-tokenizer.get_vocab()
-for idx in range(len(tokenizer.get_vocab())):
-    print(tokenizer.id_to_token(idx))
-output = tokenizer.encode("rough This parts")
-print(output.tokens)
-print(output.ids)
+# tokenizer.get_vocab()
+# for idx in range(len(tokenizer.get_vocab())):
+#     print(tokenizer.id_to_token(idx))
+# output = tokenizer.encode("rough This parts")
+# print(output.tokens)
 # print(output.ids)
-# %%
+
+# %% GET BERT
 model_name = 'bert-base-uncased'
 model = BertModel.from_pretrained(model_name)
-
-
-sentence = "Now I want to know what does this vector refers to in dictionary"
-
 bert_tokenizer = BertTokenizer.from_pretrained(model_name)
+
+# EXAMPLE TOKENISATION
+sentence = "Now I want to know what does this vector refers to in dictionary"
 tokens = bert_tokenizer.encode(sentence)
-# %%
-print(tokens)
-# print(tokens.ids)
-# print(tokens.tokens)
+# print(bert_tokenizer.ids_to_tokens)
+# TODO DOES A ROUND TRIP - ENCODING AND DECODING RESULT IN THE SAME THING?
 
 
-# %%
-# TODO DOES A ROUNT TRIP - ENCODING AND DECODING RESULT IN THE SAME THING?
+# %% GET EMBEDDINGS FROM BERT MODEL
 
+n_embeddings = 30000
 
-# TODO GET EMBEDDINGS FROM BERT MODEL
 embedding_matrix = model.embeddings.word_embeddings.weight.detach()
-embedding_matrix = embedding_matrix[:1000]
-print(embedding_matrix)
-print(embedding_matrix.shape)
 
-# %%
-# ADD NEW COLS
-labels = pd.DataFrame({"Word": bert_tokenizer.ids_to_tokens.values()})
-print(type(labels))
-labels["len"] = labels["Word"].apply(lambda word: len(word))
-
-# SAVE
-# labels.to_csv("metadata.tsv", sep="\t") # this is the format that tensorboard expects
-# %%
+embedding_matrix = embedding_matrix[:n_embeddings]
+# print(embedding_matrix)
+print("Embedding shape:", embedding_matrix.shape)
 
 
-print(dir(bert_tokenizer))
-# %%
-print(bert_tokenizer.ids_to_tokens)
+def create_embedding_labels():
+    # ADD NEW COLS
+    label_functions = {
+        "Length": lambda word: len(word),
+        "# vowels": lambda word: len([char for char in word if char in "aeiou"]),
+        "is number": lambda word: word.isdigit(),
+        "is preposition": lambda word: word in prepositions
+    }
+    labels = [
+        [
+            word,
+            *[label_function(word) for label_function in label_functions.values()]
+        ]
+        for word in list(bert_tokenizer.ids_to_tokens.values())[:n_embeddings]
+    ]
 
-# %%
-# TODO VISUALISE BERT EMBEDDING
-# visualise_embeddings(
-#     embedding_matrix,
-#     labels="metadata.tsv",
-# )
+    label_names = ["Word", *list(label_functions.keys())]
 
-# %%
+    # SAVE LABELS TSV
+    # labels.to_csv("metadata.tsv", sep="\t") # this is the format that tensorboard expects
+    return labels, label_names
+
+
+labels, label_names = create_embedding_labels()
+
+
 # LABEL BY LENGTH
-start = time()
 visualise_embeddings(
     embedding_matrix,
-    labels=[[len(word), word]
-            for word in list(bert_tokenizer.ids_to_tokens.values())[:1000]],
-    label_names=["Length", "Word"]
+    labels=labels,
+    label_names=label_names
 )
-print(f"Total time:", time() - start)
-# %%
-# def get_color():
-
-
-# def get_num_vowels(word):
-#     return len([char for char in word if char in ("a", "e", "i", "o", "u")])
-
-
-# def get_num_consonants(word):
-#     return len([char for char in word if char not in ("a", "e", "i", "o", "u")])
-
-# TODO parts of speech
-
-#
-
-# %%
-
-# var = 10
-
-# class MyClass:
-#     def my_method(self):
-#         pass
-
-#     @staticmethod
-#     def my_static_method():
-#         pass
-
-#     @classmethod
-#     def class_method(cls):
-#         pass
-
-
-# myclass = MyClass()
-# myclass.my_method()
-
-# MyClass.my_static_method()
-# myclass.my_static_method()
-
-# MyClass.class_method()
-# myclass.class_method()
 
 # %%
--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
 from torch.utils.tensorboard import SummaryWriter
+from time import time
 
 
 def visualise_embeddings(embeddings, labels=None, label_names="Label"):
     print("Embedding")
 
     writer = SummaryWriter()
+    start = time()
     writer.add_embedding(
         mat=embeddings,
         metadata=labels,
         metadata_header=label_names
     )
+    print(f"Total time:", time() - start)
+
     print("Embedding done")
