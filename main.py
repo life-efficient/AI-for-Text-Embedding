54a9bcb54f0b5aec8666eb9af0108f5db61c7c5d
Harry Berg <harryaberg@gmail.com>
2022-12-12 21:59:41 +0000

    Adds interpolation vectors between given words

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tokenizers.trainers import WordLevelTrainer
 from transformers import BertModel, BertTokenizer
 import pandas as pd
 from word_types.prepositions import prepositions
+import torch
 # %% CUSTOM TOKENISER
 # train_corpus = "pythonwiki.txt"
 
@@ -40,7 +41,8 @@ tokens = bert_tokenizer.encode(sentence)
 # print(bert_tokenizer.ids_to_tokens)
 # TODO DOES A ROUND TRIP - ENCODING AND DECODING RESULT IN THE SAME THING?
 
-
+# %%
+model.modules
 # %% GET EMBEDDINGS FROM BERT MODEL
 
 n_embeddings = 30000
@@ -50,6 +52,25 @@ embedding_matrix = model.embeddings.word_embeddings.weight.detach()
 embedding_matrix = embedding_matrix[:n_embeddings]
 # print(embedding_matrix)
 print("Embedding shape:", embedding_matrix.shape)
+
+# # %%
+
+
+# def myfunc(ag1, arg2, arg3):
+#     pass
+
+
+# a = print
+# a("hello world")
+
+# myfunc(1, 2, 3)
+# myvar = [1, 2, 3]
+# myfunc(*myvar)
+
+
+# myfunc(1, 2, 3, myarg="hello")
+# myfunc(myotherarg=0, myarg="hello")
+# %%
 
 
 def create_embedding_labels():
@@ -79,10 +100,73 @@ labels, label_names = create_embedding_labels()
 
 
 # LABEL BY LENGTH
-visualise_embeddings(
-    embedding_matrix,
-    labels=labels,
-    label_names=label_names
-)
+# visualise_embeddings(
+#     embedding_matrix,
+#     labels=labels,
+#     label_names=label_names
+# )
+
+# %%
+
+
+def get_word_embedding(word):
+
+    bert_tokenizer.tokens_to_ids = {
+        token: id for id, token in bert_tokenizer.ids_to_tokens.items()}
+
+    token_id = bert_tokenizer.tokens_to_ids[word]
+    embedding = embedding_matrix[token_id]
+    return embedding
+
+
+def analogy_solver(a, b, c, embedding_matrix, labels):
+    """
+    Solves A is to B what C is to D, given, A, B & C, returning D
+
+    """
+
+    # GET EMBEDDINGS FOR KNOWN WORDS
+    a_embedding = get_word_embedding(a)
+    b_embedding = get_word_embedding(b)
+
+    # GET TRANSFORMATION APPLIED
+    transformation_vector = b_embedding - a_embedding
+
+    # plot the embedding of the interpolation
+
+    c_embedding = get_word_embedding(c)
+    print(c_embedding.shape)
+    d_embedding = c_embedding + transformation_vector
+    print(d_embedding.shape)
+    # TODO turn trans vecotr into a word ???
+    # TODO add transformation vector to new_initial_word to get resulting embedding
+    # TODO turn resulting embedding into word
+    embedding_matrix = torch.cat(
+        (
+            embedding_matrix,
+            transformation_vector.unsqueeze(0),
+            # d_embedding.unsqueeze(0)
+        )
+    )
+
+    labels = [
+        *labels,
+        ["INTERPOLATION_VECTOR", 0, 0, 0, 0],
+        # ["D_EMBEDDING", 0, 0, 0, 0]
+    ]
+    visualise_embeddings(
+        embedding_matrix,
+        labels=labels,
+        label_names=label_names
+    )
+
+    # cosine similarity from d_embedding to embedding of all words
+    # find the smallest one = closest word = d
+    # print(f"{a} is to {b} as {c} is to {d}")
+
+
+analogy_solver("man", "woman", "king", embedding_matrix, labels)
+
+# %%
 
 # %%
